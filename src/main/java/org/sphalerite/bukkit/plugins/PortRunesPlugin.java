28449e376b9b317ad63752b8547cbc129610b486
Linus <anonymouse2048@gmail.com>
2014-06-07 11:01:20 +0200

    Bukkit plugin!

---
+++ src/main/java/org/sphalerite/bukkit/plugins/PortRunesPlugin.java
@@ -0,0 +1,23 @@
+package org.sphalerite.bukkit.plugins;
+
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class PortRunesPlugin extends JavaPlugin implements Listener {
+	@Override
+	public void onEnable() {
+		// TODO
+	}
+	
+	@Override
+	public void onDisable() {
+		// TODO
+	}
+	
+	@EventHandler
+	public void onPlayerInteractBlock(PlayerInteractEvent event) {
+		// TODO
+	}
+}
